7c1a1777579a8d4e197be45e31907856af3ea140
José <sesejose@gmail.com>
2023-01-13 15:17:00 +0100

    CSS

--- components/elements/Abouthero.js
+++ components/elements/Abouthero.js
@@ -4,9 +4,9 @@ import aboutImg from "/public/about.png";
 function Aboutsection() {
   return (
     <>
-      <section className="aboutHero">
-        <Image src={aboutImg} alt="photo of the festival" />
-        <h1 className="PINK">About Cosmo</h1>
+      <section className="about-hero">
+        {/* <Image src={aboutImg} alt="photo of the festival" /> */}
+        <h1 className="pink">About Cosmo</h1>
       </section>
     </>
   );

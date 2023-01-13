edf137985a7056c0513e53c58b384cdcc2984420
José <sesejose@gmail.com>
2023-01-13 12:07:10 +0100

    Hero

--- components/elements/Herosection.js
+++ components/elements/Herosection.js
@@ -1,21 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
-import heroImg from "/public/festivalLanding.jpg";
+// import heroImg from "/public/festivalLanding.jpg";
 
 export default function Herosection() {
   return (
     <>
       <section>
         <div className="herosection">
-          <Image src={heroImg} alt="festival" width={1512} height={830} />
-          <h2 className="pink">15/12</h2>
-
-          <h3 className="pink">
-            WILL<br></br>YOU<br></br>MISS<br></br>IT?
-          </h3>
-          <Link className="booknowBtn" value="Book now" href="/tickets">
-            BOOK NOW
-          </Link>
+          {/* <Image src={heroImg} alt="festival" width={1512} height={830} /> */}
+          <div className="dates-and-will-you-container">
+            <h2 className="pink huge">15/12</h2>
+            <div className="will-you-and-btn-container">
+              <h3 className="will-you pink">
+                WILL<br></br>YOU<br></br>MISS<br></br>IT?
+              </h3>
+              <Link className="btn-hero" href="/tickets">
+                BOOK NOW
+              </Link>
+            </div>
+          </div>
         </div>
       </section>
     </>
